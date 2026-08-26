4329f97a2ef35faeb9ff53497c81bde3365b317d
tmirhan <emirhantkr01@gmail.com>
2026-08-26 12:14:55 +0300

    Update snoozeService.js

--- snoozeService.js
+++ snoozeService.js
@@ -116,6 +116,9 @@
 
   // Power-Up kaydını sakla
   await SS.setBoardShared(t, SS.KEYS.HIDDEN_CARDS_MAP, map);
+    if (!until) {
+  await t.set('card', 'due', null);
+}
     
   return map[card.id];
 }
