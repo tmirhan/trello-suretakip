32fe3e603018f9777cda2784f3cbc05ee88f4540
tmirhan <emirhantkr01@gmail.com>
2026-08-26 11:31:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@
  */
 
 window.HMK_CONFIG = {
-  TRELLO_API_KEY: '97b1c28be3ed1475d6ab733b838fd8ab',
+  TRELLO_API_KEY: 'TEST123',
   APP_NAME: 'TRELLO HUKUKİ SÜRE HESAPLA',
   EMAIL_BACKEND_URL: '/api/send-email',
 };
