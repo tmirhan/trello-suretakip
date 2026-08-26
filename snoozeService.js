0cf79277ce832436069f9d77e3c05c1d481b2730
tmirhan <emirhantkr01@gmail.com>
2026-08-26 11:42:40 +0300

    Update snoozeService.js

--- snoozeService.js
+++ snoozeService.js
@@ -113,6 +113,8 @@
       setAtISO: new Date().toISOString(),
     };
     await SS.setBoardShared(t, SS.KEYS.HIDDEN_CARDS_MAP, map);
+    // Trello kartındaki gerçek Due Date'i kaldır
+await t.set('card', 'due', null);
     return map[card.id];
   }
 
